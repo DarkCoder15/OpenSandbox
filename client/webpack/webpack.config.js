ae2a9c1c856b5956ed7ebcd1d347ed75f49f3f8e
x7k3e21 <uly4nkin.denis@gmail.com>
2023-07-17 18:12:43 +0300

    Several fixes

--- client/webpack/webpack.config.js
+++ client/webpack/webpack.config.js
@@ -18,6 +18,9 @@ module.exports = {
             }
         ]
     },
+    resolve: {
+        extensions: [ ".js", ".ts", ".scss", ".sass" ]
+    },
     plugins: [
         htmlWebpackPlugin
     ]
